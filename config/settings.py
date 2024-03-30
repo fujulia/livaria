34af5abb745b52b186786e05e1976a74d1d596c2
fujulia <juliafu713@gmail.com>
2024-03-30 10:45:25 -0300

    correcao debug

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2ues+vyr3se9sikcs$pfsz)(2wwkst6=ao-)yyf=6zhl@==vv+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
