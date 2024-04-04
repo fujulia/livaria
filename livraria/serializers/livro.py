8146cdd50153afc592b82d04b7c60cb5aa8d5189
fujulia <juliafu713@gmail.com>
2024-04-04 15:46:26 -0300

    requirements atualizado

--- livraria/serializers/livro.py
+++ livraria/serializers/livro.py
@@ -9,15 +9,16 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
-        capa_attachment_key = SlugRelatedField(source="capa", queryset=Image.objects.all(), slug_field="attachment_key", required=False, write_only=True,)
-        capa = ImageSerializer(required=False, read_only=True)
+       
+    capa_attachment_key = SlugRelatedField(source="capa", queryset=Image.objects.all(), slug_field="attachment_key", required=False, write_only=True,)
+    capa = ImageSerializer(required=False, read_only=True)
 
 class LivroDetailSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
         depth = 1
-        capa = ImageSerializer(required=False)
+    capa = ImageSerializer(required=False)
 
 class LivroListSerializer(ModelSerializer):
     class Meta:
